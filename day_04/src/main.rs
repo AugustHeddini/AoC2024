1b9fc6303fc7f4ebe4d8822ac84418a0630f2035
August Heddini <August.Heddini@Gmail.com>
2024-12-04 12:20:50 +0100

    Add day_04 part 2

--- day_04/src/main.rs
+++ day_04/src/main.rs
@@ -4,11 +4,9 @@ use std::{
 };
 
 const SEARCH_WORD: [u8; 4] = [b'X', b'M', b'A', b'S'];
+const SEARCH_CROSS_CENTER: u8 = b'A'; 
+const SEARCH_CROSS: [u8; 2] = [b'M', b'S'];
 const DIRECTIONS: [(i16, i16); 8] = [(1, 0), (-1, 0), (0, -1), (0, 1), (1, 1), (-1, -1), (1, -1), (-1, 1)];
-
-fn into_char_list(line: &Vec<u8>) -> Vec<char> {
-    line.iter().map(|bt| *bt as char).collect::<Vec<char>>()
-}
 
 fn search_for_word(word_map: &Vec<Vec<u8>>, x: usize, y: usize) -> u8 {
     let mut count: u8 = 0;
@@ -37,23 +35,39 @@ fn search_for_word(word_map: &Vec<Vec<u8>>, x: usize, y: usize) -> u8 {
                 }
             }
         }
-        println!("found XMAS at {}, {} with direction {}, {}", x, y, dx, dy);
         count += 1;
     }
 
     return count;
 }
 
-fn main() {
-    let input = File::open("input").unwrap();
-    let reader = BufReader::new(input);
+fn search_for_cross(word_map: &Vec<Vec<u8>>, x: usize, y: usize) -> u8 {
+    let mut count = 0;
 
-    let mut word_map: Vec<Vec<u8>> = vec![];
-
-    for line in reader.lines() {
-        word_map.push(line.unwrap().as_bytes().into());
+    if word_map[x][y] != SEARCH_CROSS_CENTER {
+        return count;
     }
 
+    if 
+        (
+            SEARCH_CROSS.contains(&word_map[x-1][y-1])
+            && SEARCH_CROSS.contains(&word_map[x+1][y+1])
+            && word_map[x-1][y-1] != word_map[x+1][y+1]
+        )
+        &&
+        (
+            SEARCH_CROSS.contains(&word_map[x-1][y+1])
+            && SEARCH_CROSS.contains(&word_map[x+1][y-1])
+            && word_map[x-1][y+1] != word_map[x+1][y-1]
+        )
+    {
+        count += 1;
+    }
+
+    return count;
+}
+
+fn part1(word_map: &Vec<Vec<u8>>) {
     let mut total: u32 = 0;
     for x in 0..word_map.len() {
         for y in 0..word_map [0].len() {
@@ -62,4 +76,35 @@ fn main() {
     }
 
     println!("Total occurrences of {:?} found: {:?}", SEARCH_WORD, total);
+}
+
+fn part2(word_map: &Vec<Vec<u8>>) {
+    let mut total: u32 = 0;
+    for x in 1..word_map.len()-1 {
+        for y in 1..word_map [0].len()-1 {
+            total += search_for_cross(&word_map, x, y) as u32;
+        }
+    }
+
+    println!("Total crosses found: {:?}", total);
+}
+
+fn parse_input(filename: &str) -> Vec<Vec<u8>> {
+    let input = File::open(filename).unwrap();
+    let reader = BufReader::new(input);
+
+    let mut word_map: Vec<Vec<u8>> = vec![];
+
+    for line in reader.lines() {
+        word_map.push(line.unwrap().as_bytes().into());
+    }
+    
+    return word_map;
+}
+
+fn main() {
+    let word_map = parse_input("input");
+
+    part1(&word_map);
+    part2(&word_map);
 }
